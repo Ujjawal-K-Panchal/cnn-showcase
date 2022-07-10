17c39dfcd553bcb89a3d04805f9639c30564c8e7
Ujjawal .K. Panchal <ujjawalpanchal32@gmail.com>
2022-07-10 23:33:26 +0530

    feat: copyright notice to model.

--- src/model.py
+++ src/model.py
@@ -6,6 +6,10 @@ Description: A Convolutional Neural Network.
 Created on Sun Jul 10 2022 19:19:01 2022.
 
 @author: Ujjawal .K. Panchal
+===
+
+Copyright (c) 2022, Ujjawal Panchal.
+All Rights Reserved.
 """
 import torch
 from torch import nn
